124b2973c4ede7dee511361e3c75b4247e793091
EmilUzunov <Emil_ouzounov@mail.bg>
2017-08-19 02:41:11 +0300

    The unsaved information

--- Company/src/Company.java
+++ Company/src/Company.java
@@ -1,4 +1,23 @@
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Company {
 
+	ArrayList<Set<Employee>> branches;
+	TreeSet<Employee> emploees;
+	public Company(ArrayList<Set<Employee>> branches, TreeSet<Employee> emploees) {
+
+		this.branches = new ArrayList<Set<Employee>>();
+		this.branches.get(0).add(null);
+		this.branches.get(1).add(null);
+		this.branches.get(2).add(null);
+		this.branches.get(3).add(null);
+		this.branches.get(4).add(null);
+		this.emploees = new TreeSet<Employee>();
+	}
+	
+	
+	
+	
 }
--- Company/src/Employee.java
+++ Company/src/Employee.java
@@ -1,4 +1,46 @@
 
 public class Employee {
-
+	
+	private String name;
+	private String phone;
+	private int salary;
+	
+	
+	
+	public Employee(String name, String phone, int salary) {
+		super();
+		this.setName(name);
+		this.setPhone(phone);
+		this.setSalary(salary);
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	private void setName(String name) {
+		if(name.trim().length()>0&&!(name.equals(null))){
+		this.name = name;
+		}
+	}
+	
+	public String getPhone() {
+		return phone;
+	}
+	
+	public void setPhone(String phone) {
+		if(phone.trim().length()>0&&!(phone.equals(null))){
+		this.phone = phone;
+		}
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		if(salary>0){
+		this.salary = salary;
+		}
+	}
+	
+	
 }
