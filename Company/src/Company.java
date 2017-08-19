56caa902e1a5dcbc7f1d8ba12f8d888604b0db9a
EmilUzunov <Emil_ouzounov@mail.bg>
2017-08-19 03:11:37 +0300

    Testing

--- Company/src/Company.java
+++ Company/src/Company.java
@@ -4,19 +4,28 @@ import java.util.TreeSet;
 
 public class Company {
 
-	ArrayList<Set<Employee>> branches;
-	TreeSet<Employee> emploees;
-	public Company(ArrayList<Set<Employee>> branches, TreeSet<Employee> emploees) {
+	private ArrayList<Set<Employee>> branches;
+	private TreeSet<Employee> emploees;
+	
+	public Company() {
 
 		this.branches = new ArrayList<Set<Employee>>();
-		this.branches.get(0).add(null);
-		this.branches.get(1).add(null);
-		this.branches.get(2).add(null);
-		this.branches.get(3).add(null);
-		this.branches.get(4).add(null);
 		this.emploees = new TreeSet<Employee>();
+		this.branches.get(0).addAll(emploees);
+		this.branches.set(0, null);
+//		this.branches.get(1).add(null);
+//		this.branches.get(2).add(null);
+//		this.branches.get(3).add(null);
+//		this.branches.get(4).add(null);
+		
 	}
 	
+	public void addEmployee(int branchNumber, Employee employee){
+		this.branches.get(branchNumber).add(employee);
+	}
+	public void createBranch(String name){
+		
+	}
 	
 	
 	
--- Company/src/Demo.java
+++ Company/src/Demo.java
@@ -3,6 +3,28 @@ public class Demo {
 
 	public static void main(String[] args) {
 
+		Company myCompany = new Company();
+		
+		Employee ivan = new Employee("Ivan","0881234456",900);
+		Employee stefan = new Employee("Stefan","0881564456",950);
+		Employee krisi = new Employee("Krisi","0881238956",800);
+		Employee sara = new Employee("Sara","0881239876",980);
+		Employee marta = new Employee("Marta","0881238765",1000);
+		Employee dani = new Employee("Dani","0881212356",700);
+		Employee pedro = new Employee("Pedro","0881246556",925);
+		Employee hose = new Employee("hose","0881235956",800);
+		Employee metodi = new Employee("Metodi","0881239616",998);
+		
+		myCompany.addEmployee(0, ivan);
+		myCompany.addEmployee(0, stefan);
+		myCompany.addEmployee(0, krisi);
+		myCompany.addEmployee(0, sara);
+		myCompany.addEmployee(0, marta);
+		myCompany.addEmployee(0, dani);
+		myCompany.addEmployee(0, pedro);
+		myCompany.addEmployee(0, hose);
+		myCompany.addEmployee(0, metodi);
+		
 	}
 
 }
